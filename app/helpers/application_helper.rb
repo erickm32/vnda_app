d1de459c407c4e3b4a581f50be7aacecc71447fa
Érick Moreira <erick@sementesrastreadas.com.br>
2021-08-29 21:27:49 -0300

    Poorly adding bootstrap into project. - Surely there are a better way to do it - Updating application.html.erb with some template from bootstrap's examples - Updating the Flash container style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    classes = {
+      'notice' => 'alert alert-info',
+      'success' => 'alert alert-success',
+      'error' => 'alert alert-danger',
+      'alert' => 'alert alert-warning'
+    }
+    classes[level]
+  end
 end
