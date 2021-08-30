f6b808248db2998df9ba272ca3db38b93307b00a
Érick Moreira <erick@sementesrastreadas.com.br>
2021-08-29 22:35:22 -0300

    Small update on redirect_spec. Still have to describe the callbacks

--- spec/models/redirect_spec.rb
+++ spec/models/redirect_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Redirect, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:shop) }
+    it { is_expected.to have_many(:sync_jobs) }
   end
 
   describe 'validations' do
@@ -10,5 +11,10 @@ RSpec.describe Redirect, type: :model do
     it { is_expected.to validate_presence_of(:destination) }
 
     it { is_expected.to validate_uniqueness_of(:origin).scoped_to(:shop_id) }
+    it { is_expected.to validate_uniqueness_of(:api_id).scoped_to(:shop_id).allow_nil }
   end
+
+  # TODO: implement
+  # describe 'callbacks' do
+  # end
 end
